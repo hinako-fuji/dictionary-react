ae27e378d6f6982d1e945e34b1e57ffabad9734d
hinako-fuji <117415389+hinako-fuji@users.noreply.github.com>
2023-01-24 21:10:48 +0900

    added dictionary API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <div className="container"></div>
+      <div className="container">
+        <main>
+          <Dictionary />
+        </main>
+        <footer>This project was coded by Hinako and is open source</footer>
+      </div>
     </div>
   );
 }
